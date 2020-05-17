2739df597b7b594fd894b5641edc4bb5edc25a43
Zander <z-man88@hotmail.co.uk>
2020-05-17 13:50:33 +0100

    second draft sent

--- Code & Audio/CustomCompare.m
+++ Code & Audio/CustomCompare.m
@@ -1,35 +1,29 @@
 function [similarityCoefficient] = CustomCompare(firstNumbers,secondNumbers)
 %CorrFinder Function finds the correlation between two number sets
-%   Detailed explanation goes here
+[C,lag] = xcorr(firstNumbers,secondNumbers);
 
-        [C,lag] = xcorr(firstNumbers,secondNumbers);
+[~,I] = max(abs(C));
+SampleDiff = lag(I);
+if (SampleDiff<0)
+    CsecondNumbers = firstNumbers;
+    DfirstNumbers = secondNumbers;
+    secondNumbers = CsecondNumbers;
+    firstNumbers = DfirstNumbers;
 
-        [~,I] = max(abs(C));
-        SampleDiff = lag(I);
-        
-%         delay = finddelay(secondNumbers,firstNumbers); %not needed as SampleDiff does this
+[C,lag] = xcorr(firstNumbers,secondNumbers);
+[~,I] = max(abs(C));
+SampleDiff = lag(I);
+end
 
-        if (SampleDiff<0)
-            CsecondNumbers = firstNumbers;
-            DfirstNumbers = secondNumbers;
-            secondNumbers = CsecondNumbers;
-            firstNumbers = DfirstNumbers;
-            
-            
-        [C,lag] = xcorr(firstNumbers,secondNumbers);
-        [~,I] = max(abs(C));
-        SampleDiff = lag(I);
-        end
-        
-        if ( SampleDiff == 0 || numel(firstNumbers) == numel(secondNumbers) )
-            cc = corrcoef(secondNumbers,firstNumbers);
-            similarityCoefficient = cc(1,2);            
-        elseif (SampleDiff + numel(secondNumbers)) > numel(firstNumbers) +10 || SampleDiff<0
-            Result = "Probably not the same";     
-            similarityCoefficient = Result;
-        else
-            closestMatched = firstNumbers(SampleDiff:numel(secondNumbers) + SampleDiff -1);
-            cc = corrcoef(secondNumbers,closestMatched);
-            similarityCoefficient = cc(1,2);
-        end   
+if ( SampleDiff == 0 || numel(firstNumbers) == numel(secondNumbers) )
+    cc = corrcoef(secondNumbers,firstNumbers);
+    similarityCoefficient = cc(1,2);            
+elseif (SampleDiff + numel(secondNumbers)) > numel(firstNumbers) +10 || SampleDiff<0
+    Result = "Probably not the same";     
+    similarityCoefficient = Result;
+else
+    closestMatched = firstNumbers(SampleDiff:numel(secondNumbers) + SampleDiff -1);
+    cc = corrcoef(secondNumbers,closestMatched);
+    similarityCoefficient = cc(1,2);
+end   
 end
